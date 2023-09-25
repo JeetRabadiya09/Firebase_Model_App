c6e0dffc53d973d6196ca8a87ed9e49fbcac03a3
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-09-25 14:41:07 +0530

    complete

--- lib/view/add_todo.dart
+++ lib/view/add_todo.dart
@@ -1,79 +1,24 @@
+// !=
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-import '../../model/to_do_model.dart';
-import '../res/constant.dart';
+import '../model/to_do_model.dart';
+import '../utils/utils.dart';
 
-class AddTodo extends StatefulWidget {
-  final int? index;
-  const AddTodo({
-    super.key,
-    this.index,
-  });
+class AddEditToDoView extends StatefulWidget {
+  final ToDoModel? toDoModel;
+  final String? id;
+
+  const AddEditToDoView({super.key, this.toDoModel, this.id});
 
   @override
-  State<AddTodo> createState() => _AddTodoState();
+  State<AddEditToDoView> createState() => _AddEditToDoViewState();
 }
 
-class _AddTodoState extends State<AddTodo> {
-  TextEditingController titlecontroller = TextEditingController();
-  TextEditingController subtitlecontroller = TextEditingController();
-  TextEditingController timecontroller = TextEditingController();
-
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-  userdata() {
-    try {
-      firestore.collection('Firebase data').where("").get().then(
-        (value) {
-          debugPrint("value.size ------> ${value.size}");
-
-          for (var data in value.docs) {
-            debugPrint("value.id ------> ${data.id}");
-            debugPrint("value.docs ------> ${data.data()}");
-          }
-        },
-      );
-    } on FirebaseException catch (error) {
-      debugPrint("Firebase error------> $error");
-    } catch (error) {
-      debugPrint("error------> $error");
-    }
-  }
-
-  storeuserdata() {
-    try {
-      firestore.collection('Firebase data').add(
-        {
-          "TextEditingController": titlecontroller,
-          "TextEditingController": subtitlecontroller,
-          "TextEditingController": timecontroller
-        },
-      ).then(
-        (value) {
-          debugPrint("Data add------> ");
-        },
-      );
-    } on FirebaseException catch (error) {
-      debugPrint("Firebase error------> $error");
-    } catch (error) {
-      debugPrint("error------> $error");
-    }
-  }
-
-// @override
-// void initState() {
-//   // TODO: implement initState
-//   userdata();
-//   super.initState();
-// }
+class _AddEditToDoViewState extends State<AddEditToDoView> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentController = TextEditingController();
   String time = "";
-//
-//   SharedPreferences? prefs;
-//
-//   setInstance() async {
-//     prefs = await SharedPreferences.getInstance();
-//   }
 
   TimeOfDay timeOfDay = TimeOfDay.now();
   Future displayTimePicker(BuildContext context) async {
@@ -89,148 +34,171 @@ class _AddTodoState extends State<AddTodo> {
       });
     }
   }
-  //
-  // setTodoData() {
-  //   prefs!.setString(
-  //       "ToDoModelData",
-  //       json.encode(
-  //           Constant.toDoModelList.map((value) => value.tojson!()).toList()));
-  // }
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  SetTodo() {
+    try {
+      firestore.collection("ToDoFire").add({
+        "title": titleController.text.trim(),
+        "content": contentController.text.trim(),
+        "time": time,
+      }).then(
+        (value) {
+          Utils().showToastMessage(content: "To Do successfully add");
+          Navigator.pop(context);
+        },
+      );
+    } on FirebaseException catch (error) {
+      Utils()
+          .showSnackBar(context: context, content: "Firebase Error-->$error");
+    } catch (error) {
+      Utils().showSnackBar(context: context, content: "$error");
+    }
+  }
+
+  updateTodo() {
+    try {
+      firestore.collection("ToDoFire").doc(widget.id).update({
+        "title": titleController.text.trim(),
+        "content": contentController.text.trim(),
+        "time": time,
+      }).then(
+        (value) {
+          Utils().showToastMessage(content: "To Do successfully update");
+          Navigator.pop(context);
+        },
+      );
+    } on FirebaseException catch (error) {
+      debugPrint("Firebase error------------>$error");
+      Utils()
+          .showSnackBar(context: context, content: "Firebase Error-->$error");
+    } catch (error) {
+      Utils().showSnackBar(context: context, content: "$error");
+    }
+  }
 
   @override
   void initState() {
     // TODO: implement initState
-    // setInstance();
-    userdata();
 
-    if (widget.index != null) {
-      titlecontroller.text = Constant.toDoModelList[widget.index!].title!;
-      subtitlecontroller.text = Constant.toDoModelList[widget.index!].subtitle!;
-      time = Constant.toDoModelList[widget.index!].time!;
+    if (widget.toDoModel != null) {
+      titleController.text = widget.toDoModel!.title!;
+      contentController.text = widget.toDoModel!.title!;
+      time = widget.toDoModel!.time!;
     }
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double screenHeight = size.height;
+    double screenWidth = size.width;
+    EdgeInsets devicePadding = MediaQuery.of(context).viewPadding;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.index == null ? "Add To-do" : "Edit To-Do"),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(15),
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: titlecontroller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide:
-                      const BorderSide(width: 1, color: Colors.deepPurple),
-                ),
-                // contentPadding: const EdgeInsets.all(00),
-                isDense: true,
-                labelText: "title",
-                hintText: "Enter title ",
-                contentPadding: const EdgeInsets.all(12),
-                hintStyle: const TextStyle(
-                    color: Color(0xFFB3B3B3),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Poppins"),
-              ),
-              onTap: () {},
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: subtitlecontroller,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide:
-                      const BorderSide(width: 1, color: Colors.deepPurple),
-                ),
-                // contentPadding: const EdgeInsets.all(00),
-                isDense: true,
-                labelText: "Subtitle",
-                hintText: "Enter Subtitle ",
-                contentPadding: const EdgeInsets.all(30),
-                hintStyle: const TextStyle(
-                    color: Color(0xFFB3B3B3),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Poppins"),
-              ),
-              onTap: () {},
-            ),
-            // AppTextFiled(
-            //   controller: titleController,
-            //   hintText: "Enter Title",
-            //   textInputType: TextInputType.text,
-            // ),
-            // const SizedBox(height: 15),
-            // AppTextFiled(
-            //   controller: contentController,
-            //   hintText: "Enter Content",
-            //   textInputType: TextInputType.text,
-            // ),
-            const SizedBox(height: 15),
-            // GestureDetector(
-            //   onTap: () => displayTimePicker(context),
-            //   child: Container(
-            //     height: 45,
-            //     padding: const EdgeInsets.symmetric(horizontal: 15),
-            //     decoration: BoxDecoration(
-            //       borderRadius: BorderRadius.circular(10),
-            //       border: Border.all(color: Colors.black54, width: 1.2),
-            //     ),
-            //     child: Row(
-            //       mainAxisSize: MainAxisSize.min,
-            //       children: [
-            //         Text(time.isEmpty ? "hh : mm" : time),
-            //         const SizedBox(width: 8),
-            //         const Icon(Icons.date_range_rounded),
-            //       ],
-            //     ),
-            //   ),
-            // ),
-            const SizedBox(height: 25),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  if (widget.index != null) {
-                    //? To Edit to-do model in to-doModel list
-                    Constant.toDoModelList[widget.index!] = ToDoModelData(
-                      title: titlecontroller.text,
-                      subtitle: subtitlecontroller.text,
-                      time: timecontroller.text,
-                    );
-                    setState(() {});
-                  } else {
-                    //? To add to-do model in to-doModel list
-                    Constant.toDoModelList.add(
-                      ToDoModelData(
-                        title: titlecontroller.text,
-                        subtitle: subtitlecontroller.text,
-                        time: timecontroller.text,
-                      ),
-                    );
-                    setState(() {});
-                  }
-                  // setTodoData();
-                  Navigator.pop(context);
-                },
-                child: Text(widget.index == null ? "Add To-do" : "Edit To-Do"),
-              ),
-            ),
-          ],
+        title: Text(
+          widget.toDoModel == null ? "Add To-do" : "Edit To-Do",
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 26,
+            fontWeight: FontWeight.w700,
+          ),
         ),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.deepPurple),
+                    ),
+                    // contentPadding: const EdgeInsets.all(00),
+                    isDense: true,
+                    labelText: "title",
+                    hintText: "Enter title ",
+                    contentPadding: const EdgeInsets.all(12),
+                    hintStyle: const TextStyle(
+                        color: Color(0xFFB3B3B3),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: "Poppins"),
+                  ),
+                  onTap: () {},
+                ),
+              ),
+              const SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: TextField(
+                  controller: contentController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.deepPurple),
+                    ),
+                    // contentPadding: const EdgeInsets.all(00),
+                    isDense: true,
+                    labelText: "contant",
+                    hintText: "Enter contant ",
+                    contentPadding: const EdgeInsets.all(30),
+                    hintStyle: const TextStyle(
+                        color: Color(0xFFB3B3B3),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: "Poppins"),
+                  ),
+                  onTap: () {},
+                ),
+              ),
+              const SizedBox(height: 15),
+              GestureDetector(
+                onTap: () => displayTimePicker(context),
+                child: Container(
+                  height: 45,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.black54, width: 1.2),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(time.isEmpty ? "hh : mm" : time),
+                      const SizedBox(width: 8),
+                      const Icon(Icons.date_range_rounded),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight / 2),
+              const SizedBox(width: 10),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (widget.toDoModel == null) {
+                      SetTodo();
+                    } else {
+                      updateTodo();
+                    }
+                  },
+                  child: Text(
+                      widget.toDoModel == null ? "Add To-do" : "Edit To-Do"),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_model_app/utils/utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 import '../model/to_do_model.dart';
-import '../res/constant.dart';
 import 'add_todo.dart';
 
 class HomeView extends StatefulWidget {
@@ -23,108 +23,116 @@ class _HomeViewState extends State<HomeView> {
         title: const Text("To - Do View"),
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: firestore.collection("ToDoFire").snapshots(includeMetadataChanges: true),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
-          if (snapshot.hasError){
-            return const Center(
-              child: Text("Something went wrong............",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),),
+          stream: firestore
+              .collection("ToDoFire")
+              .snapshots(includeMetadataChanges: true),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text(
+                  "Something went wrong............",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+              return const Center(
+                child: Text(
+                  "No To-Do Found............",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              );
+            }
+
+            toDoData = List<ToDoModel>.from(snapshot.data!.docs.map((value) =>
+                ToDoModel.fromjson(value.data()! as Map<String, dynamic>)));
+            return ListView.separated(
+              itemCount: toDoData.length,
+              padding: const EdgeInsets.all(15),
+              separatorBuilder: (context, index) => const SizedBox(height: 15),
+              itemBuilder: (context, index) => Container(
+                padding: const EdgeInsets.all(15),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: CupertinoColors.lightBackgroundGray,
+                      blurRadius: 10,
+                      spreadRadius: 0,
+                      offset: Offset(0, 6),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Title: ${toDoData[index].title}",
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text("SubTitle: ${toDoData[index].content}"),
+                        ],
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        firestore
+                            .collection("ToDoFire")
+                            .doc(snapshot.data!.docs[index].id)
+                            .delete()
+                            .then((value) {
+                          toDoData.removeAt(index);
+                          Utils().showToastMessage(content: "TO Do Deleted");
+                        });
+                        setState(() {});
+                      },
+                      color: Colors.red,
+                      icon: const Icon(CupertinoIcons.delete),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddEditToDoView(
+                              id: snapshot.data!.docs[index].id,
+                              toDoModel: toDoData[index],
+                            ),
+                          ),
+                        ).then((value) {
+                          setState(() {});
+                        });
+                      },
+                      color: Colors.blue,
+                      icon: const Icon(CupertinoIcons.pen),
+                    ),
+                  ],
+                ),
+              ),
             );
-          }
-          if (snapshot.connectionState == ConnectionState.waiting){
-            return const Center(child: CircularProgressIndicator(),);
-          }
-          if
-        }
-      )
-      // Constant.toDoModelList.isEmpty
-      //     ? const Center(
-      //         child: Text(
-      //           "No To-Do Found",
-      //           style: TextStyle(
-      //             fontSize: 20,
-      //             fontWeight: FontWeight.bold,
-      //           ),
-      //         ),
-      //       )
-      //     : ListView.separated(
-      //         itemCount: Constant.toDoModelList.length,
-      //         padding: const EdgeInsets.all(15),
-      //         separatorBuilder: (context, index) => const SizedBox(height: 15),
-      //         itemBuilder: (context, index) => ListTile(
-      //           title: Text(toDoModelList[index].title ?? ""),
-      //           subtitle: Text(toDoModelList[index].subtitle ?? ""),
-      //         ),
-              //     Container(
-              //   padding: const EdgeInsets.all(15),
-              //   width: double.infinity,
-              //   decoration: BoxDecoration(
-              //     color: Colors.white,
-              //     borderRadius: BorderRadius.circular(15),
-              //     boxShadow: const [
-              //       BoxShadow(
-              //         color: CupertinoColors.lightBackgroundGray,
-              //         blurRadius: 10,
-              //         spreadRadius: 0,
-              //         offset: Offset(0, 6),
-              //       ),
-              //     ],
-              //   ),
-              //   child: Row(
-              //     children: [
-              //       Expanded(
-              //         child: Column(
-              //           crossAxisAlignment: CrossAxisAlignment.start,
-              //           children: [
-              //             Text(
-              //               "Title: ${Constant.toDoModelList[index].title}",
-              //               style: const TextStyle(
-              //                 fontSize: 16,
-              //                 fontWeight: FontWeight.bold,
-              //               ),
-              //             ),
-              //             Text(
-              //                 "SubTitle: ${Constant.toDoModelList[index].content}"),
-              //           ],
-              //         ),
-              //       ),
-              //       IconButton(
-              //         onPressed: () {
-              //           setState(() {
-              //             Constant.toDoModelList.removeAt(index);
-              //             prefs!.setString(
-              //                 "ToDoList",
-              //                 json.encode(Constant.toDoModelList
-              //                     .map((value) => value.toJson())
-              //                     .toList()));
-              //           });
-              //         },
-              //         color: Colors.red,
-              //         icon: const Icon(CupertinoIcons.delete),
-              //       ),
-              //       IconButton(
-              //         onPressed: () {
-              //           Navigator.push(
-              //             context,
-              //             MaterialPageRoute(
-              //               builder: (context) => AddTodo(index: index),
-              //             ),
-              //           ).then((value) {
-              //             setState(() {});
-              //           });
-              //         },
-              //         color: Colors.blue,
-              //         icon: const Icon(CupertinoIcons.pen),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-            ),
+          }),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => const AddTodo(),
+              builder: (context) => const AddEditToDoView(),
             ),
           ).then((value) {
             setState(() {});
