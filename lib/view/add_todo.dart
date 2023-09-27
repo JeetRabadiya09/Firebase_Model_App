a359618c98f797525d61da742e91ea770c40ed37
Jeet Rabadiya <jeetrabadiya876@gmail.com>
2023-09-27 19:09:07 +0530

    change

--- lib/view/add_todo.dart
+++ lib/view/add_todo.dart
@@ -25,6 +25,9 @@ class _AddEditToDoViewState extends State<AddEditToDoView> {
     var timeData = await showTimePicker(
       context: context,
       initialTime: timeOfDay,
+    );  var timeData = await showTimePicker(
+      context: context,
+      initialTime: timeOfDay,
     );
 
     if (timeData != null) {
